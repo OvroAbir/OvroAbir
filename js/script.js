4714f4bd43a83b035cc87781a056a5c3d7da4cb8
joy <joyghosh826@gmail.com>
2016-12-26 00:20:11 +0600

    solved scrolling active nav problem.

--- js/script.js
+++ js/script.js
@@ -12,9 +12,22 @@ $("#menu-toggle").click(function(e) {
 
 AOS.init(
 	{
-      	duration: 1000,
-      	easing: 'linear',
+      	duration: 600,
+      	easing: 'ease-out',
       	delay: 100,
 		disable: 'mobile'
 	}
 );
+
+
+var navAra = document.getElementsByClassName("jumbotronMy");
+for(var i=0;i<navAra.length;i++)
+{
+	navAra[i].addEventListener("mouseenter", function(){
+		$(".sidebar-nav").find(".nav-active").removeClass("nav-active");
+		var divId = "#nav-"+this.id;
+		//console.log("Got id "+ divId);
+		//document.getElementById(divId).parent.className = "page-scroll nav-active";
+		$(divId).parent().addClass("nav-active");
+	});
+}
