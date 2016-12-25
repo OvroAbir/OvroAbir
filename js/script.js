8a9cc50a9cf85b5c4382113e66f0e1eb19160fcd
joy <joyghosh826@gmail.com>
2016-12-25 11:33:34 +0600

    aos animation added

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@ $(".sidebar-nav a").on("click", function(){
    $(".sidebar-nav").find(".nav-active").removeClass("nav-active");
    $(this).parent().addClass("nav-active");
 });
+
+$("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+
+AOS.init(
+	{
+      	duration: 1000,
+      	easing: 'linear',
+      	delay: 100,
+		disable: 'mobile'
+	}
+);
