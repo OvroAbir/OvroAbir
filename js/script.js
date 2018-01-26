ca4088ead2a83d16c3a31420998bdb540e9c2757
Joy <joyghosh826@gmail.com>
2018-01-26 12:46:53 +0600

    intgrated timeline from codepen

--- js/script.js
+++ js/script.js
@@ -59,3 +59,10 @@ $(".jumbotronMy").scroll(function(){
   var divId = "#nav-"+$(this).attr("id");
   $(divId).parent().addClass("nav-active");
 });
+
+
+//timeline
+$(".timeline-wrapper .timeline-content-item > span").on("mouseenter mouseleave", function(e){
+  $(".timeline-wrapper .timeline-content-item.active").removeClass("active");
+  $(this).parent().addClass("active");
+});
