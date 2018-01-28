77be3690d6adcae681c7cebb68db9f36fd69d052
Joy <joyghosh826@gmail.com>
2018-01-28 14:23:00 +0600

    added timeline according to company wise

--- js/timeline.js
+++ js/timeline.js
@@ -1,14 +1,92 @@
-var currentYear = new Date().getFullYear();
-var startYear = 2015;
-var numOfYears = currentYear - startYear + 2;
-var widthOfActiveBlock = 95 / numOfYears + 5;
-var widthOfBlock = 95 / numOfYears;
+var companies = 
+[
+    {
+        name : "Kona Software Lab Limited, Bangladesh",
+        short_name : "KONA SL",
+        url : "https://www.konai.com",
+        logo : "img/kona-logo.png",
+        position : "Junior Software Engineer",
+        from : new Date("2018-02-01"),
+        to : new Date(),
+        text : "",
+        tools : ["C++"]
+    },
+    {
+        name : "CTrends Software & Services Ltd.",
+        short_name : "CTrends",
+        url : "http://www.ctrends-software.com",
+        logo : "http://www.ctrends-software.com/resources/images/logo-top.png",
+        position : "Junior Programmer",
+        from : new Date("2017-10-15"),
+        to : new Date("2018-01-15"),
+        text : "Worked on automating business processes in Enterprise Business Solution Software.",
+        tools : ["Spring MVC", "Hibernate", "HTML5", "CSS3", "Bootstrap", "jQuery", "PostgreSQL"]
+    }
+];
 
+var numOfBlocks = companies.length;
+var widthOfActiveBlock = 95 / numOfBlocks + 5;
+var widthOfBlock = 95 / numOfBlocks;
 
-for(var year=startYear;year <= currentYear;year++)
+addCompanies();
+
+function addCompanies()
 {
-    addYearBlock(year);
+    var htm = '';
+    for(var i=0;i<companies.length;i++)
+    {
+        var s = '<div id="exp-company-' + i +'" class="timeline-content-item" data-company-index="' + i + '">' +
+                    '<span>' + companies[i].short_name + '</span>' +
+                    '<div class="timeline-content-item-reveal">' +
+                        '<a href="' + companies[i].url +'" target="_blank">' +
+                            '<img src="' + companies[i].logo +'">' +
+                            //'<span>' + companies[i].short_name + '</span>' +
+                        '</a>' +
+                    '</div>' +
+                '</div>';
+        htm = htm + s;
+    }
+    $("#timeline-years").append(htm);
 }
+
+function changeCompanyBlockText(companyIndex)
+{
+    if(companyIndex == undefined)
+    {
+        console.log("undefined company index");
+        return;
+    }
+    if(companyIndex < 0)
+        return;
+    var c = companies[companyIndex];
+    $("#exp-comp-logo").attr("src", c.logo);
+    $("#exp-company-name").html(c.name);
+    $("#exp-company-name").attr("href", c.url);
+    $("#exp-position").html(c.position);
+
+    var formatter = Intl.DateTimeFormat("en-us", {year:"numeric", month:"short"});
+    var txt = "";
+    var wtxt = "";
+    var curDate = new Date();
+    if(curDate.getFullYear() == c.to.getFullYear() && curDate.getMonth() == c.to.getMonth())
+    {
+        txt = formatter.format(c.from) + " - " + "Current";
+        wtxt = "Working with "
+    }
+    else
+    {
+        txt = formatter.format(c.from) + " - " + formatter.format(c.to);
+        wtxt = "Worked with ";
+    }
+    $("#exp-time").html(txt);
+    $("#exp-resp").html(c.text);
+
+    for(var i=0;i<c.tools.length-1;i++)
+        wtxt = wtxt + c.tools[i] + ", "
+    wtxt = wtxt + c.tools[c.tools.length-1] + ".";
+    $("#exp-tool").html(wtxt);
+}
+
 
 $(".timeline-wrapper .timeline-content-item").each(function () {
     $(this).width(widthOfBlock + "%");
@@ -18,28 +96,19 @@ $(".timeline-wrapper .timeline-content-item > span").on("mouseenter mouseleave",
     $(".timeline-wrapper .timeline-content-item.active").removeClass("active");
     $(this).parent().addClass("active");
     $(this).parent().width(widthOfActiveBlock + "%");
+    changeCompanyBlockText($(this).parent().data("company-index"));
+    $("#experience-text").show(60);
 });
 
 $(".timeline-content-item").on("mouseleave", function (e) {
     $(this).removeClass("active");
     $(this).width(widthOfBlock + "%");
+    $("#experience-text").hide();
 });
 
-
-function addYearBlock(year)
-{
-    var htm = '<div class="timeline-content-item" data-timeline="hour-8">' +
-                    '<span>' + year + '</span>' +
-                    '<div class="timeline-content-item-reveal">' +
-                        '<a href="#">' +
-                            '<img src="img/hire-me.png">' +
-                            '<span>Lorem Ipsum</span>'+
-                        '</a>' +
-                    '</div>' +
-                '</div>';
-
-    //var htm = '<div class="timeline-content-item" data-timeline="hour-9"><span>9 AM</span><div class="timeline-content-item-reveal"><a href="#"><img src="img/hire-me.png"><span>Lorem Ipsum</span></a></div></div>';
-
-    $("#timeline-years").append(htm);
-}
-
+$(".timeline-content-item").on("click", function(){
+    var idx = $(this).data("company-index");
+    if(idx < 0)
+        return;
+    window.open(companies[idx].url);
+});
